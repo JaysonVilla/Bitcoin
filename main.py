7536d13d47a1d593872c411a97b50b8f254b7924
Jayson <jaysontiburciovillanueva@gmail.com>
2026-07-09 15:47:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,4 @@
-# MIT License
-# Copyright (c) 2024 Jayson
-import http.client, json, os
-from dotenv import load_dotenv
-load_dotenv()
-API_KEY = os.getenv("BLOCKCHAIN_API_KEY")
-print("API Key loaded. Ready to check BTC")
-# MIT License
-# Copyright (c) 2024 Jayson
-
-import http.client
-import json
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-API_KEY = os.getenv("BLOCKCHAIN_API_KEY")
-
-def check_btc_address(btc_address):
-    if not API_KEY:
-        print("ERROR: Walang API Key. Gumawa ka ng .env file")
-        return
-
-    conn = http.client.HTTPSConnection("api.blockchain.info")
-    headers = {'X-Explorer-Auth-Key': API_KEY}
-    payload = json.dumps({"address": btc_address})
+# loaded. Ready t
     
     conn.request("POST", "/explorer-gateway-kt/btc/address", payload, headers)
     res = conn.getresponse()
